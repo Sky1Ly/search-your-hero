d22cbd5f33344f41dbee96d782fa1d2720c4f48f
Eider Agudelo <eider841agudelo@hotmail.com>
2024-04-09 13:31:07 -0400

    Se crea estilo y base html y se crea acceso con la api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
+    let token = 4905856019427443
+
+    searchHero(token, 731)
+
     
+
+    function searchHero (token, id) {
+        $.ajax({
+            type: "GET",
+            url: `https://superheroapi.com/api.php/${token}/${id}`,
+            dataType: "json",
+            success: function (response) {
+                console.log(response)
+            }
+        });
+    }
 })
