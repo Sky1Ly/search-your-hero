efdb9904e6be2c9ca19dc87492ff8b49c12e2a6f
Eider Agudelo <eider841agudelo@hotmail.com>
2024-04-09 18:44:56 -0400

    Se muestra una card al dar clic

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,39 @@ $(document).ready(function () {
         if (isNaN(data)) {
             alert("El dato ingresado no es un número, intentelo nuevamente")
         } else {
-            if(data < 1 || data > 731) {
+            if (data < 1 || data > 731) {
                 alert("El número ingresado debe estar entre 1 y 731")
             } else {
                 event.preventDefault()
                 searchHero(token, data)
+
+                $("#contenido").html(`
+                    <div class="card" style = "width: 18rem;" >
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">An item</li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                `)
+
                 $("#inputSearch").val("")
             }
         }
     })
 
-    
 
-    function searchHero (token, id) {
+
+    function searchHero(token, id) {
         $.ajax({
             type: "GET",
             url: `https://superheroapi.com/api.php/${token}/${id}`,
