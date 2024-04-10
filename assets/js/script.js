24bd19894e628d36cf6957d4e65f214688f578e5
Eider Agudelo <eider841agudelo@hotmail.com>
2024-04-10 11:52:06 -0400

    Se renderiza información del heroe en la card

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     //El evento clic del botón debe ir async ya que al usar una función externa para el llamado de la API,
     //Esta debe ser async y al llamarla igual debe ser async, por ende; el async del boton es necesario para
     //que espere la respuesta de la API
-    $("button").on("click",async function () {
+    $("button").on("click", async function () {
         let data = $("#inputSearch").val()
 
         //Validación si el dato es un número
@@ -23,20 +23,30 @@ $(document).ready(function () {
 
                 //Pinta en pantalla la card del hero (.append aggrega el elemento al final)
                 $("#contenido").html(`
-                    <div class="card" style = "width: 18rem;" >
-                        <img src="${arrayHero.image.url}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h3 class="card-title">${arrayHero.name}</h3>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                        </ul>
-                        <div class="card-body">
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="${ arrayHero.image.url }" class="img-fluid rounded-start" alt="...">
+                            </div>
+
+                            <div class="col-md-8">
+                                <div class="card-header">
+                                    <h3 class="card-title">${ arrayHero.name }</h3>
+                                </div>
+
+                                <div class="card-body">
+                                    <p class="card-text">Connections: ${arrayHero.connections['group-affiliation']}</p>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">First appearance: ${arrayHero.biography['first-appearance']}</li>
+                                        <li class="list-group-item">Occupation: ${arrayHero.work.occupation}</li>
+                                        <li class="list-group-item">Height: ${arrayHero.appearance.height[1]}</li>
+                                    </ul>
+                                </div>
+
+                                <div class="card-footer text-body-secondary">
+                                    Publisher: ${arrayHero.biography.publisher}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 `)
@@ -55,12 +65,12 @@ $(document).ready(function () {
                 url: `https://superheroapi.com/api.php/${token}/${id}`,
                 dataType: "json"
             });
-    
+
             return response
 
         } catch (error) {
             throw error
         }
-        
+
     }
 })
